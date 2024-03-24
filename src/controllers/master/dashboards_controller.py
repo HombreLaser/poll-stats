6eb0acbf5598ee1a50384d2ff371406ceea9b7bd
HombreLaser <buran@silosneeded.com>
2024-03-24 13:14:11 -0600

    Arregla dashboard maestro

--- src/controllers/master/dashboards_controller.py
+++ src/controllers/master/dashboards_controller.py
@@ -4,4 +4,4 @@ dashboards_blueprint = Blueprint('dashboards_controller', __name__)
 
 @dashboards_blueprint.get('/master')
 def index():
-    return render_template('index.jinja')
+    return render_template('views/master/dashboards/index.jinja')
