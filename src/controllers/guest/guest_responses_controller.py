02cca71f1032140352b9c81e2ab19a16e1201ea4
HombreLaser <sebastian-440@live.com>
2024-04-25 12:16:01 -0600

    Crea formulario de respuesta

--- src/controllers/guest/guest_responses_controller.py
+++ src/controllers/guest/guest_responses_controller.py
@@ -4,6 +4,7 @@ from src.queries.shared import get_form_by_public_key
 
 
 guest_responses_blueprint = Blueprint('guest_responses_controller', __name__)
+templates_context = 'views/guest/responses/'
 
 
 @guest_responses_blueprint.get('/forms/<form_public_key>/responses/new')
@@ -11,6 +12,11 @@ def new(form_public_key):
     form = get_form_by_public_key(form_public_key)
 
     if form is not None:
-        return form.public_key
+        return render_template(f"{templates_context}/new.jinja", form=form)
     else:
-        return 'not found'
+        abort(404)
+
+
+@guest_responses_blueprint.post('/forms/<form_public_key>/responses/new')
+def create(form_public_key):
+    return 'Created'
