706c29978a4ed402731c2bea477d69a544a4a7f6
HombreLaser <sebastian-440@live.com>
2024-04-29 09:38:06 -0600

    Elimina función sin uso

--- src/queries/shared/responses_query.py
+++ src/queries/shared/responses_query.py
@@ -6,9 +6,3 @@ from src.database.models import Response, Form
 class ResponsesQuery(JoinedQueryBase):
     def get_responses(self):
         return self.search_with_foreign_models(Response, Form)
-
-    def _order_by_form_param(self):
-        if 'form' in self._params.get('order_by'):
-            return True
-
-        return False
